1aca7f4ce575524e35c54304ee61827813449b28
Geoff Hayward <git@geoffhayward.eu>
2013-10-13 20:50:33 +0100

    plgContentResponsiveMediaInstallerScript changes

--- plugins/content/responsivemedia/script.php
+++ plugins/content/responsivemedia/script.php
@@ -1,28 +1,24 @@
 <?php
 defined('_JEXEC') or die('Restricted access');
+jimport('joomla.database.databasequery');
 
-jimport( 'joomla.database.databasequery' );
+class plgContentResponsiveMediaInstallerScript {
 
-class plg_responsivemediaInstallerScript{
-     
     public function postflight($type, $parent) {
-        if ( $type == 'install'){
+        if ($type == 'install' || $type == 'discover_install'){
             $db = JFactory::getDbo();
-            try
-            {
-               $query = $db->getQuery(true);
-               $query->update("#__extensions");
-               $query->set("enabled = 1");
-               $query->where("element = 'ResponsiveMedia'", 'AND');
-               $query->where("type = 'plugin'", 'AND');
-               $query->where("folder = 'content'");
-               $db->setQuery($query);
-               $db->execute();
-            }
-            catch (Exception $exception)
-            {
-               throw $exception;
-            }
+           try {
+                $query = $db->getQuery(true);
+                $query->update("#__extensions");
+                $query->set("enabled = 1");
+                $query->where("element = 'ResponsiveMedia'", 'AND');
+                $query->where("type = 'plugin'", 'AND');
+                $query->where("folder = 'content'");
+                $db->setQuery($query);
+                $db->execute();
+           } catch (Exception $exception) {
+                throw $exception;
+           }
         }
     }
 }
