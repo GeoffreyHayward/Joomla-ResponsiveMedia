fb13550bd8f885c69e3c5d8d8b41c03d9c8ca7b8
Geoff Hayward <git@geoffhayward.eu>
2013-10-13 14:05:32 +0100

    The author's credit is now hooked up.

--- plugins/content/responsivemedia/ResponsiveMedia.php
+++ plugins/content/responsivemedia/ResponsiveMedia.php
@@ -18,11 +18,12 @@ jimport('joomla.plugin.plugin');
    @license GNU General Public License version 2 or later; see LICENSE.txt
  */
 class plgContentResponsiveMedia extends JPlugin{
-
+    
     public function onContentPrepare($context, $row, &$params, $page = 0){    
-        if(preg_match_all('/{embed:(.*?):(.*?)}/', $row->text, $matches)){          
+        if(preg_match_all('/{embed:(.*?):(.*?)}/', $row->text, $matches)){
             for($index = 0; $index < sizeof($matches[1]); $index++){
                 if(method_exists($this, $matches[1][$index])){
+                     
                    $row->text = preg_replace('/{embed:'.$matches[1][$index].':'.$matches[2][$index].'}/', $this->$matches[1][$index]($matches[2][$index]), $row->text); 
                 }
             }
@@ -32,14 +33,24 @@ class plgContentResponsiveMedia extends JPlugin{
     }
 
     private function youtube($item){
-        return "<div class='youtube-container'><iframe src='http://www.youtube.com/embed/".$item."' frameborder='0' allowfullscreen></iframe></div>";
+        return $this->credit("<div class=\"youtube-container\"><iframe src=\"http://www.youtube.com/embed/".$item."\" frameborder=\"0\" allowfullscreen></iframe></div>");
     }
     
     private function vimeo($item){
-        return "<div class='vimeo-container'><iframe src='http://player.vimeo.com/video/".$item."' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>";
+        return $this->credit("<div class=\"vimeo-container\"><iframe src=\"http://player.vimeo.com/video/".$item."\" frameborder=\"0\" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>");
     }
     
     private function instagram($item){
-        return "<div class='instagram-container'><iframe src='//instagram.com/p/".$item."/embed/' frameborder='0' scrolling='no' allowtransparency='true'></iframe></div>";
+        return $this->credit("<div class=\"instagram-container\"><iframe src=\"http://instagram.com/p/".$item."/embed/\" frameborder=\"0\" scrolling=\"no\" allowtransparency=\"true\"></iframe></div>");
+    }
+    private function credit($iframe){
+        if($this->params->get("credit")){
+            //Load the language values for JText key.
+            $lang =& JFactory::getLanguage();
+            $lang->load('plg_content_responsivemedia',JPATH_ADMINISTRATOR);
+            return "<div class=\"responsive-media-credit\">".$iframe."<p>".JText::_("PLG_CONTENT_RESPONSIVEMEDIA_CREDIT_TEXT")."</p></div>";
+        }else{
+            return $iframe;
+        }
     }
 }
