7c0c196a164d156cd9a9fb1e500ab380e191f013
Geoff Hayward <git@geoffhayward.eu>
2013-10-13 20:21:54 +0100

    Attempt at the postflight method in the install script.

--- plugins/content/responsivemedia/script.php
+++ plugins/content/responsivemedia/script.php
@@ -1,28 +1,28 @@
 <?php
 defined('_JEXEC') or die('Restricted access');
- 
-class plg_ResponsiveMediaInstallerScript
-{
-    /**
-     * Enable the plugin to true on install.
-     */
-    function postflight($type, $parent){
-        $db = JFactory::getDbo();
-        try
-        {
-           $query = $db->getQuery(true);
-           $query->update('#__extensions');
-           $query->set(array('enabled = 1'));
-           $query->where("element = 'ResponsiveMedia'", 'AND');
-           $query->where("type = 'plugin'", 'AND');
-           $query->where("folder = 'content'");
-           $db->setQuery($query);
-           $db->execute();
+
+jimport( 'joomla.database.databasequery' );
+
+class plg_responsivemediaInstallerScript{
+     
+    public function postflight($type, $parent) {
+        if ( $type == 'install'){
+            $db = JFactory::getDbo();
+            try
+            {
+               $query = $db->getQuery(true);
+               $query->update("#__extensions");
+               $query->set("enabled = 1");
+               $query->where("element = 'ResponsiveMedia'", 'AND');
+               $query->where("type = 'plugin'", 'AND');
+               $query->where("folder = 'content'");
+               $db->setQuery($query);
+               $db->execute();
+            }
+            catch (Exception $exception)
+            {
+               throw $exception;
+            }
         }
-        catch (Exception $exception)
-        {
-           throw $exception;
-        }
-        return true;
     }
 }
